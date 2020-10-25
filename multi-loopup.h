6dbe6e7db34b2a78db0ff26a1af5db78b4e28706
Leyen Qian <leyenqian@gmail.com>
2020-10-24 18:49:39 -0600

    first commit

---
+++ multi-loopup.h
@@ -0,0 +1,11 @@
+#ifndef MULTI_LOOPUP_H
+#define MULTI_LOOPUP_H
+
+#define ARRAY_SIZE            20
+#define MAX_INPUT_FILES       10
+#define MAX_RESOLVER_THREADS  10
+#define MAX_REQUESTER_THREADS 5
+#define MAX_NAME_LENGTH       1025
+#define MAX_IP_LENGTH         INET6_ADDRSTRLEN
+
+#endif
